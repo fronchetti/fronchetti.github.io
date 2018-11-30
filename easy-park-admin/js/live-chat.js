4b494535352831f53ce6f66320292b389e19090e
Felipe Fronchetti <fronchetti@usp.br>
2018-11-30 17:15:48 -0200

    Add easy-park-admin

---
+++ easy-park-admin/js/live-chat.js
@@ -0,0 +1,26 @@
+(function() {
+
+    $('.chat').slideToggle(300, 'swing');
+    $('.chat-message-counter').fadeToggle(300, 'swing');
+
+    $('#live-chat header').on('click', function() {
+
+		$('.chat').slideToggle(300, 'swing');
+		$('.chat-message-counter').fadeToggle(300, 'swing');
+
+	});
+
+	$('#card-sol').on('click', function() {
+		$('.chat').slideToggle(300, 'swing');
+		$('.chat-message-counter').fadeToggle(300, 'swing');
+
+	});
+
+	$('.chat-close').on('click', function(e) {
+
+		e.preventDefault();
+		$('#live-chat').fadeOut(300);
+
+	});
+
+}) ();
